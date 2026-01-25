47b966a1d78874e97b93d15a4186c67ff28f0126
buenorafa <rafaelbuenooo@outlook.com>
2026-01-25 00:22:12 -0300

    feat: implement order microservice with grpc and mysql

---
+++ order/cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/buenorafa/microservices/order/config"
+	"github.com/buenorafa/microservices/order/internal/adapters/db"
+	"github.com/buenorafa/microservices/order/internal/adapters/grpc"
+	"github.com/buenorafa/microservices/order/internal/application/core/api"
+)
+
+func main() {
+	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
+	if err != nil {
+		log.Fatalf("failed to connect to database. Error: %v", err)
+	}
+
+	application := api.NewApplication(dbAdapter)
+
+	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
+	grpcAdapter.Run()
+}
---
+++ order/go.mod
@@ -0,0 +1,9 @@
+module github.com/buenorafa/microservices/order
+
+go 1.25.1
+
+replace github.com/buenorafa/microservices-proto/golang/order => ../../microservices-proto/golang/order
+
+require (
+	github.com/buenorafa/microservices-proto/golang/order v0.0.0-00010101000000-000000000000
+)
