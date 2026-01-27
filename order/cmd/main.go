39210b543fd2606bc9f9cef1cd7317d6f73877cb
buenorafa <rafaelbuenooo@outlook.com>
2026-01-26 23:07:17 -0300

    adiciona resoluzcao atv 2

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/buenorafa/microservices/order/config"
 	"github.com/buenorafa/microservices/order/internal/adapters/db"
 	"github.com/buenorafa/microservices/order/internal/adapters/grpc"
+	payment_adapter "github.com/buenorafa/microservices/order/internal/adapters/payment"
 	"github.com/buenorafa/microservices/order/internal/application/core/api"
 )
 
@@ -15,7 +16,12 @@ func main() {
 		log.Fatalf("failed to connect to database. Error: %v", err)
 	}
 
-	application := api.NewApplication(dbAdapter)
+	paymentAdapter, err := payment_adapter.NewAdapter(config.GetPaymentServiceURL())
+	if err != nil {
+		log.Fatalf("failed to initialize payment stub. Error: %v", err)
+	}
+
+	application := api.NewApplication(dbAdapter, paymentAdapter)
 
 	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
 	grpcAdapter.Run()
